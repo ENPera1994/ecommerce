c714efe2e48aa568c1a9acc4f071608e81eb676e
ENPera1994 <enpera1994@gmail.com>
2024-01-17 14:58:21 -0300

    added base mercado pago api

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,8 +1,11 @@
+from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
 from django.contrib import messages
+import mercadopago
+
 
 
 def cartSummary(request):
@@ -46,3 +49,25 @@ def cartDelete(request):
         #return redirect('cart_summary')
         messages.success(request, ("Eliminado del carrito"))
         return response
+
+
+mp = mercadopago.MP(settings.MERCADO_PAGO_ACCESS_TOKEN)
+
+def procesar_pago(request):
+    if request.method == 'POST':
+        # Lógica para crear un pago en Mercado Pago
+        preference = {
+            "items": [
+                {
+                    "title": "Producto",
+                    "quantity": 1,
+                    "currency_id": "ARS",  # Moneda argentina
+                    "unit_price": 10000,  # Monto en pesos argentinos
+                }
+            ]
+        }
+
+        preference_result = mp.create_preference(preference)
+        return render(request, 'procesar_pago_mp.html', {'preference_id': preference_result['response']['id']})
+
+    return render(request, 'formulario_pago.html')
--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('cart/', include('cart.urls')),
+    path('procesar_pago/', include('procesar_pago_mp.html')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
